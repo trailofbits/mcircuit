a08ba56460a241b61dbfe9afbdc941cd5f9f6ce3
Eric Hennenfent <eric.hennenfent@trailofbits.com>
2021-07-16 16:12:20 -0700

    Add witnesses (and a test!)

--- src/export_json.rs
+++ src/export_json.rs
@@ -5,12 +5,41 @@ use serde_json::{Map, Number, Result, Value};
 
 use crate::{Operation, WireValue};
 
-const FIELD_CHAR: i32 = 101;
-const FIELD_DEG: i32 = 1;
-const GATE_MASK: i32 = 5;
-const FEAT_MASK: i32 = 20480;
+/*
+IR1 mask flags - copied and pasted, so they may get out of sync
+ */
 
-fn make_header(field_char: i32, field_deg: i32) -> Value {
+// Arithmetic Gates
+pub const ADD: u16 = 0x0001;
+pub const ADDC: u16 = 0x0002;
+pub const MUL: u16 = 0x0004;
+pub const MULC: u16 = 0x0008;
+pub const ARITH: u16 = ADD | ADDC | MUL | MULC;
+
+// Boolean Gates
+pub const XOR: u16 = 0x0100;
+pub const AND: u16 = 0x0200;
+pub const NOT: u16 = 0x0400;
+pub const BOOL: u16 = XOR | AND | NOT;
+
+// Various features
+pub const FUNCTION: u16 = 0x1000;
+pub const FOR: u16 = 0x2000;
+pub const SWITCH: u16 = 0x4000;
+pub const FOR_FUNCTION_SWITCH: u16 = FOR | FUNCTION | SWITCH;
+pub const SIMPLE: u16 = 0x0000;
+
+// Our own flags
+/// Characteristic of the Galois field (2 for boolean circuits)
+const FIELD_CHAR: u64 = 2;
+/// Field degree, should probably be 1 unless you know what you're doing
+const FIELD_DEG: u64 = 1;
+/// Tell ZK_Interface which gates we allow
+const GATE_MASK: u16 = ARITH;
+/// Tell ZK_Interface which features this circuit will use (none?)
+const FEAT_MASK: u16 = SIMPLE;
+
+fn make_header(field_char: u64, field_deg: u64) -> Value {
     Value::Object(Map::from_iter(IntoIter::new([
         ("version".to_owned(), Value::String("1.0.0".to_owned())),
         (
@@ -106,18 +135,13 @@ fn gate_to_json<T: WireValue>(gate: &Operation<T>) -> Value {
     })
 }
 
-fn arith_circuit_to_json(gates: &[Operation<u64>]) -> Result<String> {
+fn bool_circuit_to_json(gates: &[Operation<bool>], bool_witness: &[bool]) -> Result<String> {
     let ir_rep = Value::Object(Map::from_iter(IntoIter::new([
         (
             "instances".to_owned(),
             Value::Array(vec![Value::Object(Map::from_iter(IntoIter::new([
                 ("header".to_owned(), make_header(FIELD_CHAR, FIELD_DEG)),
-                (
-                    "common_inputs".to_owned(),
-                    Value::Array(vec![Value::Array(vec![
-                        // Value::Number would go here
-                    ])]),
-                ),
+                ("common_inputs".to_owned(), Value::Array(vec![])),
             ])))]),
         ),
         (
@@ -126,9 +150,19 @@ fn arith_circuit_to_json(gates: &[Operation<u64>]) -> Result<String> {
                 ("header".to_owned(), make_header(FIELD_CHAR, FIELD_DEG)),
                 (
                     "short_witness".to_owned(),
-                    Value::Array(vec![Value::Array(vec![
-                        // Value::Number would go here
-                    ])]),
+                    Value::Array(
+                        bool_witness
+                            .iter()
+                            .map(|b| {
+                                Value::Array(
+                                    b.to_le_bytes()
+                                        .iter()
+                                        .map(|u| Value::Number(Number::from(*u)))
+                                        .collect(),
+                                )
+                            })
+                            .collect(),
+                    ),
                 ),
             ])))]),
         ),
@@ -163,10 +197,27 @@ fn arith_circuit_to_json(gates: &[Operation<u64>]) -> Result<String> {
 
 #[cfg(test)]
 mod tests {
-    use crate::export_json::arith_circuit_to_json;
+    use crate::export_json::bool_circuit_to_json;
+    use crate::Operation;
 
     #[test]
     fn print_example() {
-        println!("{}", arith_circuit_to_json(&[]).unwrap());
+        println!(
+            "{}",
+            bool_circuit_to_json(
+                &[
+                    Operation::Input(1),
+                    Operation::Input(2),
+                    Operation::Input(3),
+                    Operation::Add(4, 1, 3),
+                    Operation::Add(5, 2, 3),
+                    Operation::Mul(6, 5, 4),
+                    Operation::AddConst(0, 6, true),
+                    Operation::AssertZero(0)
+                ],
+                &[false, false, true]
+            )
+            .unwrap()
+        );
     }
 }
