5e373dff26a19ef8a434f42d124d0c30c5d19a58
William Woodruff <william@trailofbits.com>
2022-09-08 18:06:50 -0400

    exporters/sieve: add gate exporting
    WIP.
    
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- src/exporters/sieve.rs
+++ src/exporters/sieve.rs
@@ -1,6 +1,6 @@
 //! Export functionality for SIEVE IRs.
 
-use std::io::{Result, Write};
+use std::io::{Error, ErrorKind, Result, Write};
 
 use crate::exporters::Export;
 use crate::Operation;
@@ -9,7 +9,52 @@ pub struct IR1;
 
 impl Export<bool> for IR1 {
     fn export_gate(gate: &Operation<bool>, sink: &mut impl Write) -> Result<()> {
-        unimplemented!();
+        match gate {
+            Operation::Input(_) => {
+                Err(Error::new(ErrorKind::Other, "can't use input gates in IR1"))
+            }
+            Operation::Random(_) => {
+                // TODO(ww): Is this true?
+                Err(Error::new(
+                    ErrorKind::Other,
+                    "can't use random gates in IR1",
+                ))
+            }
+            Operation::Add(o, l, r) => {
+                writeln!(sink, "${} <- @xor(${}, ${});", o, l, r)
+            }
+            Operation::AddConst(o, i, c) => {
+                // NOTE(ww): This could be optimized the way we do for
+                // Bristol Fashion: inv when nonzero and just an identity
+                // assign when zero.
+                writeln!(sink, "${} <- @xor(${}, < {} >);", o, i, c)
+            }
+            Operation::Sub(o, l, r) => {
+                writeln!(sink, "${} <- @xor(${}, ${});", o, l, r)
+            }
+            Operation::SubConst(o, i, c) => {
+                // NOTE(ww): This could be optimized the way we do for
+                // Bristol Fashion: inv when nonzero and just an identity
+                // assign when zero.
+                writeln!(sink, "${} <- @xor(${}, < {} >);", o, i, c)
+            }
+            Operation::Mul(o, l, r) => {
+                writeln!(sink, "${} <- @and(${}, ${});", o, l, r)
+            }
+            Operation::MulConst(o, i, c) => {
+                // NOTE(ww): This could be optimized the way we do for
+                // Bristol Fashion: inv when zero and just an identity
+                // assign when nonzero.
+                writeln!(sink, "${} <- @and(${}, < {} >);", o, i, c)
+            }
+            Operation::AssertZero(w) => {
+                writeln!(sink, "@assert_zero(${});", w)
+            }
+            Operation::Const(w, c) => {
+                writeln!(sink, "${} <- < {} >;", w, c)
+            }
+            _ => unimplemented!(),
+        }
     }
 
     fn export_circuit(
