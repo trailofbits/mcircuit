59ad1ed2b01e8702b09a5094d7a38b821b71deb0
Jack Leightcap <jack.leightcap@trailofbits.com>
2023-03-10 15:41:54 -0500

    Sized witness Export trait parameter
    `Witness` type

--- src/exporters/mod.rs
+++ src/exporters/mod.rs
@@ -3,14 +3,17 @@ use std::io::{Result, Write};
 use crate::{Operation, WireValue};
 
 mod bristol;
-mod json;
 mod sieve;
 mod sievephase2;
 
 pub use bristol::BristolFashion;
-pub use json::bool_circuit_to_json;
 pub use sieve::IR1;
 pub use sievephase2::IR0;
+
+const WITNESS_LEN: usize = 656;
+
+type Step = [bool; WITNESS_LEN];
+type Witness = Vec<Step>;
 
 /// The core export trait.
 ///
@@ -19,5 +22,9 @@ pub use sievephase2::IR0;
 pub trait Export<T: WireValue> {
     fn export_gate(gate: &Operation<T>, sink: &mut impl Write) -> Result<()>;
 
-    fn export_circuit(gates: &[Operation<T>], witness: &[T], sink: &mut impl Write) -> Result<()>;
+    fn export_circuit(
+        gates: &[Operation<T>],
+        witness: &Witness,
+        sink: &mut impl Write,
+    ) -> Result<()>;
 }
